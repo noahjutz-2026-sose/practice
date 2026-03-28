eeb72b17f40135be76fbc018ad261ce0a9ac19c1
Noah Jutz <noah@noahjutz.com>
2026-03-28 10:23:56 +0100

    Fix error in code

--- OS/ue04/memdump.c
+++ OS/ue04/memdump.c
@@ -3,7 +3,7 @@
 void tester(int *c, int k) { printf("x[%d] = %d\n", k, c[k]); }
 
 int main(int argc, char *argv[]) {
-  int i, k, x[1000];
+  int i, k, x[10000];
   for (i = 0; i < 10000; ++i)
     x[i] = i;
   printf("Enter integer in 0..9999:");
