a089974e7c9b429b6675379988f56608290ff301
Noah Jutz <noah@noahjutz.com>
2026-04-10 16:10:42 +0200

    Read from file in C

--- OS/ue07/subprocess.c
+++ OS/ue07/subprocess.c
@@ -1,8 +1,10 @@
+#include <ctype.h>
 #include <pthread.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 void strToUpper(char *sPtr) {
@@ -13,31 +15,44 @@ void strToUpper(char *sPtr) {
 }
 
 int main() {
-    char buffer[50];
+  char buffer[50];
 
-    int req[2], res[2];
-    pipe(req);
-    pipe(res);
+  int req[2], res[2];
+  pipe(req);
+  pipe(res);
 
-    int pid = fork();
+  int pid = fork();
 
-    if (pid < 0) {
-        exit(1);
-    }
+  if (pid < 0) {
+    exit(1);
+  }
 
-    if (pid == 0) {
-        close(req[1]);
-        close(res[0]);
+  if (pid == 0) {
+    close(req[1]);
+    close(res[0]);
 
-        read(req[0], buffer, 50);
+    while (read(req[0], buffer, 50) > 0) {
         strToUpper(buffer);
         write(res[1], buffer, 50);
-    } else {
-        close(req[0]);
-        close(res[1]);
+    }
+  } else {
+    close(req[0]);
+    close(res[1]);
 
-        write(req[1], "Hi there", 9);
-        read(res[0], buffer, 50);
+    FILE *file = fopen("input.txt", "r");
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+      write(req[1], buffer, strlen(buffer) + 1);
+    }
+
+    fclose(file);
+    close(req[1]);
+
+    while (read(res[0], buffer, 50) > 0) {
         printf("%s\n", buffer);
     }
+
+    close(res[0]);
+    wait(NULL);
+  }
 }
