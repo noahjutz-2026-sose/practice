b272e4903507888812c772da1218ac201ad347be
Noah Jutz <noah@noahjutz.com>
2026-04-18 16:00:23 +0200

    Implement ILP w ortools

--- OR/ue03/main.py
+++ OR/ue03/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from ue03!")
+from ortools.linear_solver import pywraplp
 
+solver: pywraplp.Solver = pywraplp.Solver.CreateSolver("SAT")
 
-if __name__ == "__main__":
-    main()
+if not solver:
+    exit(1)
+
+infinity = solver.infinity()
+x1 = solver.IntVar(0, 2, "x1")
+x2 = solver.IntVar(0, 2, "x2")
+x3 = solver.IntVar(0, 2, "x3")
+x4 = solver.IntVar(0, 2, "x4")
+x5 = solver.IntVar(0, 2, "x5")
+
+solver.Add(5 * x1 + 4 * x2 + 2 * x3 + 2 * x4 + 2 * x5 <= 15)
+
+solver.Maximize(3 * x1 + 6 * x2 + 6 * x3 + 8 * x4 + 1 * x5)
+
+status = solver.Solve()
+
+if status == pywraplp.Solver.OPTIMAL:
+    print("Solution:")
+    print("Objective value =", solver.Objective().Value())
+    print(
+        f"x1={x1.solution_value()} x2={x2.solution_value()} x3={x3.solution_value()} x4={x4.solution_value()}"
+    )
+else:
+    print("The problem does not have an optimal solution.")
