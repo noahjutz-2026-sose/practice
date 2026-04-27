9505990bfd6112db046e78b7261cca8bfd50b2fc
Noah Jutz <noah@noahjutz.com>
2026-04-27 08:53:18 +0200

    Fix errors

--- OS/ue08/bug1.c
+++ OS/ue08/bug1.c
@@ -1,32 +1,31 @@
 /******************************************************************************
-* FILE: bug1.c
-* DESCRIPTION:
-*   This example has a bug. It is a variation on the condvar.c example.
-*   Instead of just one thread waiting for the condition signal, there are
-*   four threads waiting for the same signal. Find out how to fix the
-*   program. The solution program is bug1fix.c.
-* SOURCE: Adapted from example code in "Pthreads Programming", B. Nichols
-*   et al. O'Reilly and Associates.
-* LAST REVISED: 07/06/05  Blaise Barney
-******************************************************************************/
+ * FILE: bug1.c
+ * DESCRIPTION:
+ *   This example has a bug. It is a variation on the condvar.c example.
+ *   Instead of just one thread waiting for the condition signal, there are
+ *   four threads waiting for the same signal. Find out how to fix the
+ *   program. The solution program is bug1fix.c.
+ * SOURCE: Adapted from example code in "Pthreads Programming", B. Nichols
+ *   et al. O'Reilly and Associates.
+ * LAST REVISED: 07/06/05  Blaise Barney
+ ******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-#define NUM_THREADS  6
+#define NUM_THREADS 6
 #define TCOUNT 10
 #define COUNT_LIMIT 12
 
-int     count = 0;
+int count = 0;
 pthread_mutex_t count_mutex;
 pthread_cond_t count_threshold_cv;
 
-void *inc_count(void *idp)
-{
-  int j,i;
-  double result=0.0;
+void *inc_count(void *idp) {
+  int j, i;
+  double result = 0.0;
   long my_id = (long)idp;
-  for (i=0; i < TCOUNT; i++) {
+  for (i = 0; i < TCOUNT; i++) {
     pthread_mutex_lock(&count_mutex);
     count++;
 
@@ -36,19 +35,20 @@ void *inc_count(void *idp)
     */
     if (count == COUNT_LIMIT) {
       pthread_cond_signal(&count_threshold_cv);
-      printf("inc_count(): thread %ld, count = %d  Threshold reached.\n", my_id, count);
-      }
-    printf("inc_count(): thread %ld, count = %d, unlocking mutex\n", my_id, count);
+      printf("inc_count(): thread %ld, count = %d  Threshold reached.\n", my_id,
+             count);
+    }
+    printf("inc_count(): thread %ld, count = %d, unlocking mutex\n", my_id,
+           count);
     pthread_mutex_unlock(&count_mutex);
 
     /* Do some work so threads can alternate on mutex lock */
     sleep(1);
-    }
+  }
   pthread_exit(NULL);
 }
 
-void *watch_count(void *idp)
-{
+void *watch_count(void *idp) {
   long my_id = (long)idp;
 
   printf("Starting watch_count(): thread %ld\n", my_id);
@@ -61,22 +61,21 @@ void *watch_count(void *idp)
   from never returning.
   */
   pthread_mutex_lock(&count_mutex);
-    printf("***Before cond_wait: thread %ld\n", my_id);
-    pthread_cond_wait(&count_threshold_cv, &count_mutex);
-    printf("***Thread %ld Condition signal received.\n", my_id);
+  printf("***Before cond_wait: thread %ld\n", my_id);
+  pthread_cond_wait(&count_threshold_cv, &count_mutex);
+  printf("***Thread %ld Condition signal received.\n", my_id);
   pthread_mutex_unlock(&count_mutex);
   pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int i, rc;
   pthread_t threads[6];
   pthread_attr_t attr;
 
   /* Initialize mutex and condition variable objects */
   pthread_mutex_init(&count_mutex, NULL);
-  pthread_cond_init (&count_threshold_cv, NULL);
+  pthread_cond_init(&count_threshold_cv, NULL);
 
   /*
   For portability, explicitly create threads in a joinable state
@@ -94,12 +93,11 @@ int main(int argc, char *argv[])
   for (i = 0; i < NUM_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
-  printf ("Main(): Waited on %d  threads. Done.\n", NUM_THREADS);
+  printf("Main(): Waited on %d  threads. Done.\n", NUM_THREADS);
 
   /* Clean up and exit */
   pthread_attr_destroy(&attr);
   pthread_mutex_destroy(&count_mutex);
   pthread_cond_destroy(&count_threshold_cv);
-  pthread_exit (NULL);
-
+  pthread_exit(NULL);
 }
