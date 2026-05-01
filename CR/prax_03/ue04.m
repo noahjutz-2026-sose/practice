657dc67b6d66121231709202bab6faec4b4e10a9
Noah <noah.jutz@st.oth-regensburg.de>
2026-05-01 13:58:41 +0000

    implement ieee any length

--- CR/prax_03/ue04.m
+++ CR/prax_03/ue04.m
@@ -1,30 +1,54 @@
-function [s, c, m, str] = IEEE754Custom(x, nc, nm)
+function [s, c, e, b, m, str] = IEEE754Custom(x, nc, nm)
     bits = false(1, nm);
     i = 1;
     intPart = floor(x);
     while intPart >= 1
         bit = mod(intPart, 2) ~= 0;
         bits(i) = bit;
-        intPart = floor(intPart / 2);
         i = i + 1;
+        intPart = floor(intPart / 2);
     end
     bits(1:i-1) = flip(bits(1:i-1));
-    e = i;
+    e = i - 1;
 
     fracPart = sym(x) - floor(sym(x));
-    while fracPart > 0 && i < nm
+    while fracPart > 0 && i <= nm + 1
         fracPart = fracPart * 2;
         bit = fracPart >= 1;
-        bits(i) = bit;
+        if ~bits(1) && ~bit
+            e = e - 1;
+        else
+            bits(i) = bit;
+            i = i + 1;
+        end
         fracPart = fracPart - floor(fracPart);
-        i = i + 1;
     end
-    str = sprintf('%d', bits);
-    disp(str);
-    disp(e);
+
+    bits = bits(2:end);
+
+    b = 2^(nc-1)-1;
+    c = e + b;
+    m = bits' * 2.^(numel(bits)-1:-1:0);
+    s = x < 0;
+
+    str = [s, logical(int2bit(c, nc))', bits];
+    str = sprintf('%d', str);
+
+
+    % str = sprintf('%d', bits);
+    % disp(str);
+    % disp(e);
 end
 
-IEEE754Custom(100.100, 0, 100)
+[s, c, e, b, m, str] = IEEE754Custom(.000000100, 8, 23);
+
+disp(s)
+disp(c)
+disp(e)
+disp(b)
+disp(str)
+
+[s, c, e, b, m, str] = IEEE754Custom(700, 10, 100);
 
 function [s, c, m, str] = IEEE754Float(x)
     
