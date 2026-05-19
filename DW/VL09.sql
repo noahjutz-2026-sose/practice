b3f5f4e211f341e99930dbad4a849456e9268119
Noah Jutz <noah@noahjutz.com>
2026-05-19 08:39:48 +0200

    bump

--- DW/VL09.sql
+++ DW/VL09.sql
@@ -1,3 +1,5 @@
+-- DataGrip + OTH Exasol instance
+
 OPEN SCHEMA NOAH_JUTZ;
 
 CREATE TABLE test(name VARCHAR(500));
@@ -5,3 +7,10 @@ CREATE TABLE test(name VARCHAR(500));
 INSERT INTO TEST VALUES ('NOAHHHH\');
 
 DROP TABLE test;
+
+CREATE TABLE gesis_tmp(
+    respid INT,
+    studyid INT
+);
+
+-- IMPORT INTO gesis_tmp FROM LOCAL CSV FILE '~/Downloads/gesis_df_v2.tsv';
