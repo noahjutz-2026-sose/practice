6600fbf2111c54474c568ff175282d544a5993af
Noah Jutz <noah@noahjutz.com>
2026-05-23 09:42:06 +0200

    Remove done todos

--- DW/challenges/ch3_staging_area.sql
+++ DW/challenges/ch3_staging_area.sql
@@ -17,15 +17,15 @@ CREATE TABLE st_Bundestag_Elections (
 );
 
 CREATE TABLE st_Politbarometer_Survey (
-    respondent_id INT, -- PK respondent, "respid"
+    respondent_id INT, -- PK respondent; "respid"
     year INT,
     month INT,
     study_id INT, -- PK respondent
     version INT, -- "split"
-    p_weight DOUBLE, -- "pwght" TODO add to M/ER
-    d_weight DOUBLE, -- "dwght" TODO add to M/ER
+    p_weight DOUBLE, -- Weight before 1999; "pwght"
+    d_weight DOUBLE, -- Weight from 1999; "dwght"
     v4a_east_west INT,
-    v5_turnout INT, -- TODO add to M/ER
+    v5_turnout INT,
     v6_intended_vote INT,
     v7_last_vote INT,
     v8_rating_spd INT,
@@ -35,42 +35,42 @@ CREATE TABLE st_Politbarometer_Survey (
     v12_rating_gruene INT,
     v13_rating_afd INT,
     v14_rating_linke INT,
-    v15_rating_government INT, -- TODO add to M/ER
-    v16_rating_opposition INT, -- TODO add to M/ER
-    v18_democracy_satisfaction INT, -- TODO translate in M/ER
-    v20_political_interest INT, -- TODO translate in M/ER
-    v21_political_interest_intensity INT, -- TODO add to M/ER
-    v22_left_right INT, -- TODO translate in M/ER
-    v23_left INT, -- TODO translate in M/ER
-    v24_right INT, -- TODO translate in M/ER
-    v25_economy_brd INT, -- TODO translate in M/ER
-    v26_economy_forecast INT, -- TODO add to M/ER
-    v27_financial_standing INT, -- TODO change financial_situation to financial_standing in M/ER
-    v28_financial_standing_forecast INT, -- TODO add to M/ER
-    v29_reunification INT, -- TODO add to M/ER
-    v30_asylum INT, -- TODO translate in M/ER
-    v31_foreigners INT, -- TODO add to M/ER
-    v32_abortion INT, -- TODO translate in M/ER
-    v39_nuclear_energy INT, -- TODO translate in M/ER
-    v41_crime_threat INT, -- TODO translate in M/ER
-    v42_eu_membership INT, -- TODO add to M/ER
-    v43_responsibility_foreign_policy INT, -- TODO add to M/ER
-    v44_society INT, -- TODO add to M/ER
-    v48_military_threat INT, -- TODO translate in M/ER
-    v49_security INT, -- TODO add to M/ER
-    v50_year_review INT, -- TODO translate in M/ER
-    v51_year_forecast INT, -- TODO translate in M/ER
-    v52_religion INT, -- TODO add dimension to M/ER
-    v54_gender INT, -- TODO rename in M/ER
+    v15_rating_government INT,
+    v16_rating_opposition INT,
+    v18_democracy_satisfaction INT,
+    v20_political_interest INT,
+    v21_political_interest_intensity INT,
+    v22_left_right INT,
+    v23_left INT,
+    v24_right INT,
+    v25_economy_brd INT,
+    v26_economy_forecast INT,
+    v27_financial_standing INT,
+    v28_financial_standing_forecast INT,
+    v29_reunification INT,
+    v30_asylum INT,
+    v31_foreigners INT,
+    v32_abortion INT,
+    v39_nuclear_energy INT,
+    v41_crime_threat INT,
+    v42_eu_membership INT,
+    v43_responsibility_foreign_policy INT,
+    v44_society INT,
+    v48_military_threat INT,
+    v49_security INT,
+    v50_year_review INT,
+    v51_year_forecast INT,
+    v52_religion INT,
+    v54_gender INT,
     v55_age INT,
-    v56_age_group INT, -- TODO add to M/ER
+    v56_age_group INT,
     v57_marital_status INT,
-    v59_education_pre_87 INT, -- TODO add to M/ER
+    v59_education_pre_87 INT,
     v60_education INT,
     v64_employment_status INT,
     v65_occupation INT,
-    v72_preferred_party INT, -- TODO rename in M/ER
-    v73_preference_intensity INT, -- TODO add to M/ER
+    v72_preferred_party INT,
+    v73_preference_intensity INT,
     v74_workers_union INT,
     v75_state INT -- TODO translate in M/ER
 );
