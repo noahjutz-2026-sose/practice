646d480ddcb459e2267bcdffbfda7abaa2e98be3
Noah <noah.jutz@st.oth-regensburg.de>
2026-05-28 15:27:06 +0000

    [CR] [AI] prax5.3.2

--- CR/prax_05/ue03.m
+++ CR/prax_05/ue03.m
@@ -14,10 +14,21 @@ xlabel('Zeit (s)'); ylabel('Beschl (m/s^2)');
 legend('x', 'y', 'z');
 title('Komponentenverlauf');
 
-figure;
-histogram(ax); hold on;
-histogram(ay);
-histogram(az);
-histogram(norm);
-legend('x', 'y', 'z', 'norm');
-title('Verteilung');
+% figure;
+% histogram(ax); hold on;
+% histogram(ay);
+% histogram(az);
+% histogram(norm);
+% legend('x', 'y', 'z', 'norm');
+% title('Verteilung');
+
+% b. mean and var
+
+mu_x = mean(ax); var_x = var(ax);
+mu_y = mean(ay); var_y = var(ay);
+mu_z = mean(az); var_z = var(az);
+
+hold on;
+yline(mu_x, 'r--', 'Mean X'); yline(mu_x + sqrt(var_x), 'r:'); yline(mu_x - sqrt(var_x), 'r:');
+yline(mu_y, 'g--', 'Mean Y'); yline(mu_y + sqrt(var_y), 'g:'); yline(mu_y - sqrt(var_y), 'g:');
+yline(mu_z, 'b--', 'Mean Z'); yline(mu_z + sqrt(var_z), 'b:'); yline(mu_z - sqrt(var_z), 'b:');
