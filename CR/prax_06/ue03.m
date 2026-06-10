b09fcef6e0e1bc08d13c2fd8bf688ff87a6c7bb2
Noah <noah.jutz@st.oth-regensburg.de>
2026-06-10 16:04:45 +0000

    [CR] prax6.3.1

--- CR/prax_06/ue03.m
+++ CR/prax_06/ue03.m
@@ -3,3 +3,24 @@ data2 = load("data/Datensatz2.mat");
 data3 = load("data/Datensatz3.mat");
 data4 = load("data/Datensatz4.mat");
 
+% Data1
+
+figure
+x = data1.xMeasure;
+y = data1.yMeasure;
+plot(x, y, 'o');
+
+% Data1 Linear model
+
+res = [x' x'.^0] \ y';
+m = res(1);
+b = res(2);
+
+hold on;
+
+idx = linspace(0, 10);
+plot(idx, m*idx + b, 'DisplayName', 'Linear');
+legend;
+
+% Data1 Polynomial model
+
