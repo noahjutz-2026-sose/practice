86c627e635f177030921ea34b11328728b1422b5
Noah <noah.jutz@st.oth-regensburg.de>
2026-06-19 14:44:22 +0000

    [CR] Th09.3 newton

--- CR/theorie_09/ue03.m
+++ CR/theorie_09/ue03.m
@@ -3,9 +3,11 @@ ys = [2 3 1 2]';
 
 n = 3;
 
-function l = l(x, j, n, xs, ys)
+% Lagrange
+
+function l = lagrange(x, j, n, xs, ys)
     l = 1;
-    for k=0:n-1
+    for k=0:n
         if k == j
             continue;
         end
@@ -15,16 +17,46 @@ function l = l(x, j, n, xs, ys)
     end
 end
 
-function y = interp(x, n, xs, ys)
+function y = interp_l(x, n, xs, ys)
     y = 0;
     for i=0:n
         y = y + ys(i + 1) * l(x, i, n, xs, ys);
     end
 end
 
+
+
+% Newton
+
+function omega = newton(x, j, xs)
+    omega = 1;
+    for i=0:j-1
+        omega = omega * (x - xs(i+1));
+    end
+end
+
+function d = divdiff(xs, ys, i, j)
+    if i == j
+        d = ys(i+1);
+        return;
+    end
+    l = divdiff(xs, ys, i+1, j);
+    r = divdiff(xs, ys, i, j-1);
+    d = (l-r)/(xs(j+1)-xs(i+1));
+end
+
+function y = interp_n(x, n, xs, ys)
+    y = 0;
+    for i=0:n
+        coeff = divdiff(xs, ys, 0, i);
+        omega = newton(x, i, xs);
+        y = y + coeff * omega;
+    end
+end
+
 figure;
 xValues = linspace(-1, 3, 100);
-yValues = arrayfun(@(x) interp(x, n, xs, ys), xValues);
+yValues = arrayfun(@(x) interp_n(x, n, xs, ys), xValues);
 plot(xValues, yValues, 'b-', 'DisplayName', 'interp');
 hold on;
 scatter(xs, ys, 'ro');
